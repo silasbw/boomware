db5b15e779f4747e96de9f92354ca63a9c02460e
JacopoDaeli <jacopo.daeli@gmail.com>
2018-07-27 20:53:09 -0700

    refactor(perfs): remove NOT operation

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function wrap (fn) {
   return function (req, res, next) {
     Promise.resolve(fn(req, res, next))
       .catch(err => {
-        if (!err.isBoom) return next(boom.badImplementation(err))
-        next(err)
+        if (err.isBoom) return next(err)
+        next(boom.badImplementation(err))
       })
   }
 }
